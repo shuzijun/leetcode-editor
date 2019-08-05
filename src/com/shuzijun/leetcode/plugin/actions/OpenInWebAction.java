6937860caf71d54c7481e6ea6fd547c303484125
shuzijun <shuzijun0109@gmail.com>
2019-08-05 19:16:57 +0800

    add a new feature of browsing certain question on the website of leetcode

--- src/com/shuzijun/leetcode/plugin/actions/OpenInWebAction.java
+++ src/com/shuzijun/leetcode/plugin/actions/OpenInWebAction.java
@@ -1,26 +1,23 @@
 package com.shuzijun.leetcode.plugin.actions;
 
-import com.alibaba.fastjson.JSON;
 import com.intellij.ide.BrowserUtil;
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.project.Project;
-import com.shuzijun.leetcode.plugin.manager.CodeManager;
 import com.shuzijun.leetcode.plugin.model.Config;
 import com.shuzijun.leetcode.plugin.model.Question;
 import com.shuzijun.leetcode.plugin.utils.DataKeys;
-import com.shuzijun.leetcode.plugin.utils.MessageUtils;
-import com.shuzijun.leetcode.plugin.utils.PropertiesUtils;
 import com.shuzijun.leetcode.plugin.utils.URLUtils;
 
 import javax.swing.*;
 import javax.swing.tree.DefaultMutableTreeNode;
 
-public class OpenInLeetCodeAction extends AbstractAction {
+/**
+ * @author zzdcon
+ */
+public class OpenInWebAction extends AbstractAction {
     @Override public void actionPerformed(AnActionEvent anActionEvent, Config config) {
         JTree tree = anActionEvent.getData(DataKeys.LEETCODE_PROJECTS_TREE);
         DefaultMutableTreeNode note = (DefaultMutableTreeNode) tree.getLastSelectedPathComponent();
         Question question = (Question) note.getUserObject();
-        BrowserUtil.browse(URLUtils.getLeetcodeUrl()+"/problems/"+question.getTitleSlug());
+        BrowserUtil.browse(URLUtils.getLeetcodeProblems()+question.getTitleSlug());
     }
 }
