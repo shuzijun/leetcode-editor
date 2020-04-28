32dd7ccb98eacba9d1d5ae768c2d655cdc6a18dd
Xpol Wan <xpolife@gmail.com>
2020-04-28 13:25:26 +0800

    Add pascalCaseName and leftPadZeros.

--- src/main/java/com/shuzijun/leetcode/plugin/utils/VelocityTool.java
+++ src/main/java/com/shuzijun/leetcode/plugin/utils/VelocityTool.java
@@ -9,6 +9,46 @@ public class VelocityTool extends StringUtils {
 
     private static String[] numsAry = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
 
+    public static String leftPadZeros(String s, int resultLength) {
+        if (s.length() >= resultLength) {
+            return s;
+        }
+        int nPads = resultLength - s.length();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < nPads; ++i) {
+            sb.append('0');
+        }
+        sb.append(s);
+        return sb.toString();
+    }
+
+    public static String pascalCaseName(String underscoreName) {
+        if (isNotBlank(underscoreName)) {
+            underscoreName = underscoreName.replace(" ", "_");
+            StringBuilder result = new StringBuilder();
+            if (isNumeric(underscoreName.substring(0, 1))) {
+                underscoreName = numsAry[Integer.valueOf(underscoreName.substring(0, 1))] + "-" + underscoreName.substring(1);
+            }
+            boolean flag = false;
+            for (int i = 0; i < underscoreName.length(); i++) {
+                char ch = underscoreName.charAt(i);
+                if ('_' == ch || '-' == ch) {
+                    flag = true;
+                } else {
+                    if (flag) {
+                        result.append(Character.toUpperCase(ch));
+                        flag = false;
+                    } else {
+                        result.append(ch);
+                    }
+                }
+            }
+            return result.toString();
+        } else {
+            return underscoreName;
+        }
+    }
+
     public static String camelCaseName(String underscoreName) {
 
         if (isNotBlank(underscoreName)) {
