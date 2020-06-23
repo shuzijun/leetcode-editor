ed32e36ef094ae4de28bad0d8bf46e3955bcabd3
shuzijun <shuzijun0109@gmail.com>
2020-06-23 20:08:09 +0800

    Change the icon

---
+++ src/main/java/icons/LeetCodeEditorIcons.java
@@ -0,0 +1,37 @@
+package icons;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+/**
+ * @author shuzijun
+ */
+public interface LeetCodeEditorIcons {
+    
+    Icon LEETCODE_TOOL_WINDOW = IconLoader.getIcon("/icons/LeetCode.png");
+
+    Icon CLEAN = IconLoader.getIcon("/icons/clean.png");
+    Icon CLEAR = IconLoader.getIcon("/icons/clear.png");
+    Icon COLLAPSE = IconLoader.getIcon("/icons/collapse.png");
+    Icon CONFIG = IconLoader.getIcon("/icons/config.png");
+    Icon DESC = IconLoader.getIcon("/icons/desc.png");
+    Icon EDIT_DOC = IconLoader.getIcon("/icons/edit_doc.png");
+    Icon FAVORITE = IconLoader.getIcon("/icons/favorite.png");
+    Icon FILTER = IconLoader.getIcon("/icons/filter.png");
+    Icon FIND = IconLoader.getIcon("/icons/find.png");
+    Icon HELP = IconLoader.getIcon("/icons/help.png");
+    Icon HISTORY = IconLoader.getIcon("/icons/history.png");
+    Icon LOGIN = IconLoader.getIcon("/icons/login.png");
+    Icon LOGOUT = IconLoader.getIcon("/icons/logout.png");
+    Icon POPUP = IconLoader.getIcon("/icons/popup.png");
+    Icon PROGRESS = IconLoader.getIcon("/icons/progress.png");
+    Icon QUESTION = IconLoader.getIcon("/icons/question.png");
+    Icon RANDOM = IconLoader.getIcon("/icons/random.png");
+    Icon REFRESH = IconLoader.getIcon("/icons/refresh.png");
+    Icon RUN = IconLoader.getIcon("/icons/run.png");
+    Icon SOLUTION = IconLoader.getIcon("/icons/solution.png");
+    Icon SUBMIT = IconLoader.getIcon("/icons/submit.png");
+    Icon TIME = IconLoader.getIcon("/icons/time.png");
+
+}
