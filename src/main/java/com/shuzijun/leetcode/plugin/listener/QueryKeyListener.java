b878b2edcd1992f50c8cf699a0c111bc0a1c07fb
bestcondition <admin@bestcondition.cn>
2024-03-29 01:17:52 +0800

    feat: press alt to open code from url

--- src/main/java/com/shuzijun/leetcode/plugin/listener/QueryKeyListener.java
+++ src/main/java/com/shuzijun/leetcode/plugin/listener/QueryKeyListener.java
@@ -4,6 +4,7 @@ import com.intellij.openapi.progress.ProgressIndicator;
 import com.intellij.openapi.progress.ProgressManager;
 import com.intellij.openapi.progress.Task;
 import com.intellij.openapi.project.Project;
+import com.shuzijun.leetcode.plugin.manager.CodeManager;
 import com.shuzijun.leetcode.plugin.manager.NavigatorAction;
 import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.NotNull;
@@ -13,6 +14,8 @@ import org.slf4j.LoggerFactory;
 import javax.swing.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 /**
  * @author shuzijun
@@ -48,6 +51,24 @@ public class QueryKeyListener implements KeyListener {
                     navigatorAction.loadServiceData();
                 }
             });
+        }
+        if (e.getKeyCode() == KeyEvent.VK_ALT) {
+            ProgressManager.getInstance().run(new Task.Backgroundable(project, "Open code from URL", false) {
+                @Override
+                public void run(@NotNull ProgressIndicator progressIndicator) {
+                    String selectText = jTextField.getText();
+                    try {
+                        URL url = new URL(selectText);
+                        String path = url.getPath();
+                        String[] pathSegments = path.split("/");
+                        String titleSlug = pathSegments[2];
+                        CodeManager.openCode(titleSlug, project);
+                    } catch (MalformedURLException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                }
+            });
+
         }
     }
 
